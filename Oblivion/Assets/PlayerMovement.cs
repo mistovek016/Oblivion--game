bc01581463ea896cda57067b56aea1e4510d1dfe
A26-Projects <aryaman.aisola@gmail.com>
2021-05-08 10:48:40 +0530

    Movement and Sprites

---
+++ Oblivion/Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movespeed = 2f;
+
+    public Rigidbody2D rb;
+
+    Vector2 movement;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Movement
+        rb.MovePosition(rb.position + movement * movespeed);
+    }
+}
