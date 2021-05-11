d60fa2d4274e143a03b546d04c48b2ba6e527c33
Abhay <abhay.ayyasola@gmail.com>
2021-05-11 13:28:13 +0530

    Add asteroid break animation

--- Oblivion/Assets/ObstacleScript.cs
+++ Oblivion/Assets/ObstacleScript.cs
@@ -8,18 +8,25 @@ public class ObstacleScript : MonoBehaviour
 
     public int damage = 1;
 
+    private Animator animator;
+
     private void OnTriggerEnter2D(Collider2D  other)
     {
         if (other.CompareTag("Player"))
         {
             other.GetComponent<PlayerMovement>().health -= damage;
             Debug.Log(other.GetComponent<PlayerMovement>().health);
-            Destroy(gameObject);
+            animator.Play("Asteroid-1-Breaking");
         }
     }
 
     private void Update()
     {
+        animator = gameObject.GetComponent<Animator>();
         transform.Translate(Vector2.down * speed * Time.deltaTime);
+        if (gameObject.transform.position.y < -5.5)
+        {
+            Destroy(gameObject);
+        }
     }
 }
