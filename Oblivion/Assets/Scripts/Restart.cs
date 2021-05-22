4a94d097cffdc088e7d21c1231c343f5089ed920
A26-Projects <aryaman.aisola@gmail.com>
2021-05-22 14:58:46 +0530

    Audio and settings

---
+++ Oblivion/Assets/Scripts/Restart.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+
+    // Update is called once per frame
+    public void Restart1()
+    {
+        
+            
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        
+    }
+
+    public void NextLevel()
+    {
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+}
---
+++ Oblivion/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public int score;
+
+    public Text ScoreDisplay;
+
+    public Text ScoreDisplay1;
+
+    public GameObject LevelComplete;
+
+    public GameObject Blob;
+
+    public GameObject Score;
+
+    public GameObject Healthbar;
+
+    public int scorelimit = 20;
+
+    
+
+    private void Update()
+    {
+        ScoreDisplay.text = score.ToString();
+        ScoreDisplay1.text = score.ToString();
+        
+
+        if (score >= scorelimit)
+        {
+
+            LevelComplete.SetActive(true);
+            Destroy(gameObject);
+            Destroy(Blob);
+            Destroy(Healthbar);
+            Destroy(Score);
+
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+
+            score++;
+            Debug.Log(score);
+            
+
+        }
+
+        
+    }
+}
