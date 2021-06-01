19467a36dca490f3b59d6d97f7e78a9265abbe50
A26-Projects <aryaman.aisola@gmail.com>
2021-06-01 13:37:26 +0530

    Start Screen, Mute button, Ui design and more levels

---
+++ Oblivion/Assets/Scripts/IsAudioOn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsAudioOn : MonoBehaviour
+{
+
+
+    public GameObject Audio;
+
+    public int AudioPref;
+
+
+
+    void Start()
+    {
+        
+     AudioPref = PlayerPrefs.GetInt("isaudioon");
+     Debug.Log(AudioPref + "blah");
+
+     if (AudioPref == 1) {
+
+         Audio.SetActive(true);
+         
+
+     }
+
+     if (AudioPref == 0) {
+
+         Audio.SetActive(false);
+
+     }
+
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
--- Oblivion/Assets/Scripts/Settingsmenu.cs
+++ Oblivion/Assets/Scripts/Settingsmenu.cs
@@ -6,11 +6,49 @@ using UnityEngine.Audio;
 public class Settingsmenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    public GameObject audioSource;
+
+    public GameObject settings;
+
+    
 
     public void SetVolume(float volume)
     {
-
+        //Debug.Log(volume);   
         audioMixer.SetFloat("volume", volume);
 
     }
+
+    public void SetAudio(bool isAudioOn)
+    {
+
+        audioSource.SetActive(isAudioOn);
+
+        if (isAudioOn == true) {
+
+           PlayerPrefs.SetInt("isaudioon", 1);
+
+        }
+
+        if (isAudioOn == false) {
+
+           PlayerPrefs.SetInt("isaudioon", 0);
+
+        }
+
+    
+    }
+
+    public void Mute() {
+
+           AudioListener.pause = !AudioListener.pause;
+
+        }
+
+        public void Settings() {
+
+           settings.SetActive(true);
+
+        }
+
 }
